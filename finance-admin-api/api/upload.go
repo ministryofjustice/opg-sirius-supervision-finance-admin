4310132659ba0124f5d7a4657bbdaaa8e417a9d3
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2024-09-19 13:21:58 +0100

    PFS-143 Add test upload file code

--- finance-admin-api/api/upload.go
+++ finance-admin-api/api/upload.go
@@ -1,11 +1,39 @@
 package api
 
 import (
-	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/opg-sirius-finance-admin/finance-admin-api/session"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func (s *Server) upload(w http.ResponseWriter, r *http.Request) error {
-	fmt.Println("Hello :)")
+	sess, err := session.NewSession()
+
+	endpoint := os.Getenv("AWS_S3_ENDPOINT")
+	sess.AwsSession.Config.Endpoint = &endpoint
+	sess.AwsSession.Config.S3ForcePathStyle = aws.Bool(true)
+
+	uploader := s3manager.NewUploader(sess.AwsSession)
+
+	file := struct {
+		FileName     string
+		FileContents string
+	}{
+		"Test.txt",
+		"Test Contents",
+	}
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("async-upload"),
+		Key:    &file.FileName,
+		Body:   strings.NewReader(file.FileContents),
+	})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
