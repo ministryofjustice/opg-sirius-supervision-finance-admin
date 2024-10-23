a6839d5058c4736a37a4ecc3fb79c135265e0328
Sam Joseph <denialanderror@googlemail.com>
2024-10-23 16:41:14 +0100

    pfs-162: Simple filename encode

--- finance-admin/internal/server/auth.go
+++ finance-admin/internal/server/auth.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/ministryofjustice/opg-go-common/telemetry"
 	"net/http"
+	"net/url"
 )
 
 type Authenticator struct {
@@ -22,28 +23,10 @@ func (a *Authenticator) Authenticate(next http.Handler) http.Handler {
 		}
 		if !sessionValid {
 			logger.Info("User session not valid. Redirecting.")
-			http.Redirect(w, r, fmt.Sprintf("%s/auth", a.EnvVars.SiriusURL), http.StatusFound)
+			http.Redirect(w, r, fmt.Sprintf("%s/auth?redirect=%s", a.EnvVars.SiriusURL, url.QueryEscape(a.EnvVars.Prefix+r.URL.Path)), http.StatusFound)
 			return
 		}
-
-		//// Exchange session for JWT token
-		//jwtToken := exchangeSessionForJWT()
-		//
-		//// Attach the JWT token to the request context
-		//ctx := context.WithValue(r.Context(), jwtContextKey, jwtToken)
 
 		next.ServeHTTP(w, r)
 	})
 }
-
-// Helper function to get JWT token from context
-//func getJWTFromContext(ctx context.Context) (string, bool) {
-//	jwtToken, ok := ctx.Value(jwtContextKey).(string)
-//	return jwtToken, ok
-//}
-
-// Simulate session to JWT exchange
-//func exchangeSessionForJWT() string {
-//	// Placeholder for actual JWT generation
-//	return "generated-jwt-token"
-//}
