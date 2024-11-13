7612888643735954d2f813a0eff0a9ffaad8ecb6
Sam Joseph <denialanderror@googlemail.com>
2024-11-13 13:17:09 +0000

    pfs-162: Updates redirect to enclude full URI, so query params are captured

--- finance-admin/internal/server/auth.go
+++ finance-admin/internal/server/auth.go
@@ -20,12 +20,12 @@ func (a *Authenticator) Authenticate(next http.Handler) http.Handler {
 		sessionValid, err := a.Client.CheckUserSession(ctx)
 		if err != nil {
 			logger.Error("Error validating session.", "error", err)
-			http.Redirect(w, r, a.redirectPath(r.URL.Path), http.StatusFound)
+			http.Redirect(w, r, a.redirectPath(r.URL.RequestURI()), http.StatusFound)
 			return
 		}
 		if !sessionValid {
 			logger.Info("User session not valid. Redirecting.")
-			http.Redirect(w, r, a.redirectPath(r.URL.Path), http.StatusFound)
+			http.Redirect(w, r, a.redirectPath(r.URL.RequestURI()), http.StatusFound)
 			return
 		}
 
@@ -34,5 +34,5 @@ func (a *Authenticator) Authenticate(next http.Handler) http.Handler {
 }
 
 func (a *Authenticator) redirectPath(to string) string {
-	return fmt.Sprintf("%s/auth?redirect=%s", a.EnvVars.SiriusPublicURL, url.QueryEscape(fmt.Sprintf("%s/%s", a.EnvVars.Prefix, to)))
+	return fmt.Sprintf("%s/auth?redirect=%s", a.EnvVars.SiriusPublicURL, url.QueryEscape(fmt.Sprintf("%s%s", a.EnvVars.Prefix, to)))
 }
