d2a1d82d27ce06eab74d3d0070d06f39666ed6cf
Sam Joseph <denialanderror@googlemail.com>
2024-12-13 13:21:24 +0000

    pfs-196: Aged debt by customer tested. DateHelper struct added so tests are independent of the current date.

--- finance-admin-api/db/testhelpers/seeder.go
+++ finance-admin-api/db/testhelpers/seeder.go
@@ -10,9 +10,12 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"log"
 	"net/http"
+	"time"
 )
 
-var baseURL = "http://localhost:8282"
+var (
+	baseURL = "http://localhost:8282"
+)
 
 // Seeder contains a test database connection pool and HTTP server for API calls
 type Seeder struct {
@@ -77,4 +80,48 @@ func (s *Seeder) SendDataToAPI(ctx context.Context, method string, endpoint stri
 	}
 
 	return resp, nil
+}
+
+type DateHelper struct {
+	date time.Time
+}
+
+func (s *Seeder) Today() DateHelper {
+	date := DateHelper{date: time.Now()}
+	return date
+}
+
+func (t DateHelper) Add(years int, months int, days int) DateHelper {
+	t.date = t.date.AddDate(years, months, days)
+	return t
+}
+
+func (t DateHelper) Sub(years int, months int, days int) DateHelper {
+	t.date = t.date.AddDate(-years, -months, -days)
+	return t
+}
+
+func (t DateHelper) String() string {
+	return t.date.Format("2006-01-02")
+}
+
+func (t DateHelper) StringPtr() *string {
+	s := t.date.Format("2006-01-02")
+	return &s
+}
+
+func (t DateHelper) Date() time.Time {
+	return t.date
+}
+
+func (t DateHelper) DatePtr() *time.Time {
+	d := t.date
+	return &d
+}
+
+func (t DateHelper) FinancialYear() string {
+	if t.date.Month() >= time.April {
+		return fmt.Sprintf("%d/%s", t.date.Year(), t.date.AddDate(1, 0, 0).Format("06"))
+	}
+	return fmt.Sprintf("%d/%s", t.date.Year()-1, t.date.Format("06"))
 }
