ec3fffa3907ba125ae41df78db567c2af36336dc
Sam Joseph <denialanderror@googlemail.com>
2025-02-06 13:41:22 +0000

    pfs-166: Updates wording on validation

--- finance-admin/internal/server/translate.go
+++ finance-admin/internal/server/translate.go
@@ -16,8 +16,8 @@ var validationMappings = map[string]map[string]pair{
 	},
 	"Date": {
 		"Date":             pair{"Date", "Please select the report date"},
-		"date-in-the-past": pair{"Date", "The report date in the past"},
-		"min-go-live":      pair{"Date", "The report date on or after 1st April 2025"},
+		"date-in-the-past": pair{"Date", "The report date must be in the past"},
+		"min-go-live":      pair{"Date", "The report date must be on or after 1st April 2025"},
 	},
 	"FromDate": {
 		"FromDate": pair{"FromDate", "Date From must be before Date To"},
