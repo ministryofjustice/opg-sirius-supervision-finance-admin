0600956a2057f1f695d8f0a8b434d0a682d8bb7d
Sam Joseph <denialanderror@googlemail.com>
2025-05-19 11:49:08 +0100

    pfs-300: Merged in CSV header validation and renamed some params

--- cypress/e2e/upload.cy.js
+++ cypress/e2e/upload.cy.js
@@ -5,47 +5,47 @@ describe("Uploading Files", () => {
 
     describe("Upload file", () => {
         it("Uploads file successfully", () => {
-            cy.get('[data-cy=\"report-upload-type\"]').select('PAYMENTS_MOTO_CARD');
+            cy.get('[data-cy=\"upload-type\"]').select('PAYMENTS_MOTO_CARD');
             cy.get('#file-upload').selectFile('cypress/fixtures/feemoto_01102024normal.csv');
             cy.get('#upload-date').type("2024-10-01");
-            cy.get('.govuk-button').contains('Upload report').click();
+            cy.get('.govuk-button').contains('Upload file').click();
             cy.url().should("include","/uploads?success=upload");
             cy.get('.moj-banner').contains('File successfully uploaded');
         });
 
         it("Validates missing upload date", () => {
-            cy.get('[data-cy=\"report-upload-type\"]').select('PAYMENTS_MOTO_CARD');
+            cy.get('[data-cy=\"upload-type\"]').select('PAYMENTS_MOTO_CARD');
             cy.get('#file-upload').selectFile('cypress/fixtures/feemoto_01102024normal.csv');
-            cy.get('.govuk-button').contains('Upload report').click();
+            cy.get('.govuk-button').contains('Upload file').click();
             cy.get('#f-UploadDate').contains('Please enter a date');
             cy.get('#f-UploadDate').should('have.class', 'govuk-form-group--error');
         });
 
         it("Validates missing file", () => {
-            cy.get('[data-cy=\"report-upload-type\"]').select('PAYMENTS_MOTO_CARD');
-            cy.get('.govuk-button').contains('Upload report').click();
+            cy.get('[data-cy=\"upload-type\"]').select('PAYMENTS_MOTO_CARD');
+            cy.get('.govuk-button').contains('Upload file').click();
             cy.get('.govuk-error-summary').contains('No file uploaded');
             cy.get('#f-FileUpload').contains('No file uploaded');
             cy.get('#f-FileUpload').should('have.class', 'govuk-form-group--error');
         });
 
         it("Validates empty headers", () => {
-            cy.get('[data-cy=\"report-upload-type\"]').select('PAYMENTS_MOTO_CARD');
+            cy.get('[data-cy=\"upload-type\"]').select('PAYMENTS_MOTO_CARD');
             cy.get('#file-upload').selectFile('cypress/fixtures/feemoto_01122024normal.csv');
             cy.get('#upload-date').type("2024-12-01");
-            cy.get('.govuk-button').contains('Upload report').click();
+            cy.get('.govuk-button').contains('Upload file').click();
             cy.get('.govuk-error-summary').contains('Failed to read CSV headers');
             cy.get('#f-FileUpload').contains('Failed to read CSV headers');
             cy.get('#f-FileUpload').should('have.class', 'govuk-form-group--error');
         });
 
         it("Validates CSV headers", () => {
-            cy.get('[data-cy=\"report-upload-type\"]').select('PAYMENTS_MOTO_CARD');
+            cy.get('[data-cy=\"upload-type\"]').select('PAYMENTS_MOTO_CARD');
             cy.get('#file-upload').selectFile('cypress/fixtures/feemoto_02102024normal.csv');
             cy.get('#upload-date').type("2024-10-02");
-            cy.get('.govuk-button').contains('Upload report').click();
-            cy.get('.govuk-error-summary').contains('CSV headers do not match for the report trying to be uploaded');
-            cy.get('#f-FileUpload').contains('CSV headers do not match for the report trying to be uploaded');
+            cy.get('.govuk-button').contains('Upload file').click();
+            cy.get('.govuk-error-summary').contains('CSV headers do not match for the file being uploaded');
+            cy.get('#f-FileUpload').contains('CSV headers do not match for the file being uploaded');
             cy.get('#f-FileUpload').should('have.class', 'govuk-form-group--error');
         });
     });
@@ -67,7 +67,7 @@ describe("Uploading Files", () => {
 
         it("Displays the correct fields for selection", () => {
             for (const [upload, fields] of Object.entries(selections)) {
-                cy.get('[data-cy=\"report-upload-type\"]').select(upload);
+                cy.get('[data-cy=\"upload-type\"]').select(upload);
                 fields.forEach((field) => {
                    cy.get(field).should('be.visible');
                 });
