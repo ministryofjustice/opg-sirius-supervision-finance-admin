106fca14d6a6696c341ce0f1f3204071d5af4706
Kate Smith <kate.smith5@justice.gov.uk>
2026-02-11 09:35:22 +0000

    sfs-361 add annual billing page finance admin

--- finance-admin/internal/server/tab_annual_invoicing_letters.go
+++ finance-admin/internal/server/tab_annual_invoicing_letters.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -13,6 +15,13 @@ type AnnualInvoicingLettersTabHandler struct {
 }
 
 func (h *AnnualInvoicingLettersTabHandler) render(v AppVars, w http.ResponseWriter, r *http.Request) error {
+	resp, err := h.Client().AnnualBillingLetters(r.Context())
+	if err != nil {
+		log.Printf("Error calling download API: %v", err)
+		http.Error(w, "Failed to stream file", http.StatusInternalServerError)
+	}
+	fmt.Print("resp")
+	fmt.Print(resp.Body)
 	data := GetAnnualInvoicingLettersVars{v}
 	data.selectTab("annual-invoicing-letters")
 	return h.execute(w, r, data)
